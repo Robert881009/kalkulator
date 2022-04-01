16441b6ac592cc85e1da6f3ced8954a7a9c4664b
Robert881009 <robert8810@o2.pl>
2022-04-01 19:45:15 +0200

    first commit

--- src/main/java/com/example/kalkulator/Calculator.java
+++ src/main/java/com/example/kalkulator/Calculator.java
@@ -1,4 +1,25 @@
 package com.example.kalkulator;
 
 public class Calculator {
+    int a;
+    int b;
+    int result;
+
+    public void addition(int a, int b){
+        result = a + b;
+        System.out.println(result);
+    }
+    public void subtraction(int a, int b){
+        result = a - b;
+        System.out.println(result);
+    }
+}
+class Main{
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addition(10,5);
+        calculator.subtraction(25,8);
+
+    }
 }
